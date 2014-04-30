7a1f5d911ebed9e076932801d6c59455b16c7783
Jeroen Van den Berghe <vandenberghe.jeroen@gmail.com>
2014-04-30 17:07:37 +0200

    navigation

--- js/src/main.js
+++ js/src/main.js
@@ -1,1 +1,35 @@
-console.log("Hello World!");
+'use strict';
+
+var App = (function(){
+	App.toggleMenu().init();
+});
+
+App.toggleMenu = (function(){
+
+	var init = function() {
+
+		var toggleMenu = document.querySelectorAll('.js-toggle-menu')[0];
+		toggleMenu.addEventListener("click", function(){
+			toggle(toggleMenu);
+		}, false);
+	}
+
+	function toggle(elem) {
+		
+		var navigation = document.querySelectorAll('.nav__list-primary')[0];
+
+		if (navigation.className.indexOf('hidden-phone') > -1) {
+			navigation.className = navigation.className.replace('hidden-phone', '');
+		} else {
+			navigation.className = navigation.className + ' hidden-phone';
+		}
+
+	}
+
+	return {
+		init:init
+	}
+	
+});
+
+App();
